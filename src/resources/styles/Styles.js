c0dedb4be138aa43352eef534332863a860f2487
MahmoudAbdAlKareem <mahmood20771@gmail.com>
2019-04-10 07:23:23 -0700

    System push changes

--- src/resources/styles/Styles.js
+++ src/resources/styles/Styles.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
-            const TabsColor = '#4f4f8e';
-            const iconColor = '#d86e6e';
+            const TabsColor = '#445852';
+            const iconColor = '#ffffff';
             const TabsStyle = StyleSheet.create({
                 backgroundColor: {
                     backgroundColor: TabsColor
